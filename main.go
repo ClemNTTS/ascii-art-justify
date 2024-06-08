b9ab347ae6c17d22ae0cb823812d18591ef8b51d
cnuttens <clement.nutt@gmail.com>
2024-06-08 12:50:05 +0200

    first

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"lib"
+)
+
+func main() {
+	//check arguments
+	wrong_input, opt_template, opt_justify, opt_output, opt_color, sentence, asset_file_name := lib.CheckOptions()
+	if wrong_input {
+		fmt.Println("Error : Wrong input, check the README file to know how to use this project ")
+		return
+	}
+
+	fmt.Printf("template : %v\n", opt_template)
+	fmt.Printf("output : %v\n", opt_output)
+	fmt.Printf("justify : %v\n", opt_justify)
+	fmt.Printf("color : %v\n", opt_color)
+
+	asset, thinkertoy := lib.GetFileContent("assets/" + asset_file_name)
+	table_asset := lib.CreateTable(asset, thinkertoy)
+	lib.PrintAscii(table_asset, sentence)
+}
